4a7d97d6b6f6ec674933cb01466ce6a8746b1124
Trajy <henrique.trajano.a@gmail.com>
2026-07-15 17:19:49 -0300

    project generated from spring initialzr

---
+++ src/main/java/br/com/trajy/delivery/infra/identity/IdentityApplication.java
@@ -0,0 +1,13 @@
+package br.com.trajy.delivery.infra.identity;
+
+import static org.springframework.boot.SpringApplication.run;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdentityApplication {
+
+	public static void main(String[] args) {
+        run(IdentityApplication.class, args);
+	}
+
+}
